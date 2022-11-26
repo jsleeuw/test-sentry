768c1a60a6d8460d25d3cfb0a7a7f1153d0c7332
jonathan sleeuw <jsleeuw@gmail.com>
2022-11-26 09:15:31 -0500

    change

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function HomePage() {
       <button
         type="button"
         onClick={() => {
-          throw new Error("A Sentry Frontend Error")
+          throw new Error("A Sentry Frontend Error On Development")
         }}
       >
         Throw error
